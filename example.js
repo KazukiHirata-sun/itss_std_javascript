8b7ce75b609e9efa91953ba122d7835f43a8a2a9
HungNhOnPV <anhhungx9propro@gmail.com>
2020-03-12 12:02:11 +0700

    homework

--- example.js
+++ example.js
@@ -3,3 +3,19 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+
+let number;
+let arr = [];
+
+for(let i = 2; i <= 6; i++) {
+	number = Number(process.argv[i]);
+	arr.push(number);
+}
+
+arr.sort((a, b) => {
+	if(a > b) return 1;
+	else if(a < b) return -1;
+	else return 0;
+});
+
+console.log(arr);
