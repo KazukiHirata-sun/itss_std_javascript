b03b2138d8eaf47e58250f4cdc2d3210b9a3f4de
thoabk98 <hoangthithoa09041998@gmail.com>
2020-03-12 17:30:24 +0700

    find primes from input array

--- example.js
+++ example.js
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
 
 'use strict';
+let arr = []
+for(let i = 2; i < process.argv.length; i++){
+  arr.push(process.argv[i])
+}
 
-// このファイルを修正して、プルリクエストしてください。
+const isPrime = num => {
+  for(let i = 2; i <= Math.sqrt(num); i++)
+    if(num % i === 0) return false;
+  return num > 1;
+}
+
+let primes = []
+for( let i = 0; i< arr.length; i ++){
+  if(isPrime(arr[i])) {
+    primes.push(arr[i])
+  }
+}
+console.log(primes)
