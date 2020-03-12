bb1b4bf91ba65fd9a851193f1856cf6be69a9c21
QUANG NGUYEN VIET NGOC <nnquang@fresher.local>
2020-03-12 15:05:31 +0700

    課題(3)

--- example.js
+++ example.js
@@ -3,3 +3,15 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+var argv = process.argv.slice(2);
+var result = [];
+loop: for (const arg of argv) {
+    let i = 2;
+    while (i < arg) {
+        if (arg % i === 0)
+            continue loop;
+        i++;
+    }
+    result.push(arg);
+}
+console.log(result);
