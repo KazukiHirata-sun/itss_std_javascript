19f88274198ff58958d95576ca6e0878a720522e
hungking98 <hungking98@gmail.com>
2020-03-12 14:21:24 +0700

    課題(2)

--- example.js
+++ example.js
@@ -3,3 +3,15 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+let array = [];
+const n = process.argv.length
+for (let i = 2; i < n; i++) {
+
+    let tmp = Number(process.argv[i]);
+    array.push(tmp);
+    array.sort(function(a, b) { return a - b });
+    //array.sort();
+}
+
+
+console.log(array);
