3224f42490a6f1cf9dff1c9856a847c007a42117
minhhieuhd98 <hieu.nm161539@sis.hust.edu.vn>
2020-03-13 18:40:53 +0000

    課題2：完了

--- example.js
+++ example.js
@@ -3,3 +3,12 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+const compare = (a, b) => {
+    if(a > b) return 1;
+    else if(a < b) return -1;
+    else return 0;
+}
+
+var input = process.argv.slice(2);
+
+console.log(input.sort(compare));
