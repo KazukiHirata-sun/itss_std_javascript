520c0d5e35d7d12d9ccf1015b4aa8f3495f9c09c
Bach Do <dohuybach714@gmail.com>
2020-03-13 16:31:58 +0000

    課題3

--- example.js
+++ example.js
@@ -3,3 +3,26 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+const input = process.argv.slice(2);
+var res = [];
+var isPrime = (i) => {
+    if (i === 2) {
+        return true;
+    }
+    if (i < 2 || i % 2 === 0) {
+        return false;
+    }
+    for (let m = 3; m <= Math.floor(Math.sqrt(i)); m += 2) {
+        if (i % m == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+for (const value of input) {
+    let i = parseInt(value);
+    if (isPrime(i) === true) {
+        res.push(i);
+    }
+}
+console.log(res);
