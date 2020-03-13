e8623f0796adfdcc886075d1b47b6776fefc7e22
honghanh2608 <nguyenthihonghanh2608@gmail.com>
2020-03-13 09:47:05 +0700

    final

--- example.js
+++ example.js
@@ -3,3 +3,9 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+let args = process.argv.slice(2);
+let length = args.length;
+for (let i = 0; i < length; i++) {
+	let index = length - 1 - i;
+	console.log(`${index + 1}番目は${args[index]}です`);
+}
