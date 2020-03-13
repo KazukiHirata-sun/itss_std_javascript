8509b5bb1edb09b3c774bdeaba1842611458d18e
nguyenduong2612 <nguyenduong261298@gmail.com>
2020-03-13 23:51:16 +0700

    課題3

--- example.js
+++ example.js
@@ -3,3 +3,16 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+let result = []
+
+for (const arg of process.argv.slice(2)) {
+    let i = 2;
+    while (arg % i != 0) {
+        i++;
+    }
+    if (i == arg) {
+        result.push(Number(arg))
+    }
+}
+
+console.log(result)
