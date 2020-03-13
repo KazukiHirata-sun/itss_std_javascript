8ecf66673a875a59ca14ab25e6e8966be0c1f949
グイン・マイン・ティエン <tiennguyenbka198@gmail.com>
2020-03-13 15:03:18 +0000

    Done Kadai 3

--- example.js
+++ example.js
@@ -2,4 +2,23 @@
 
 'use strict';
 
-// このファイルを修正して、プルリクエストしてください。
+//ユーザが入力できるのは数字のみとする
+
+const primes=[];
+
+for (var arg of process.argv.slice(2)) {
+    arg=Number(arg);
+    if (arg < 2) continue; 
+    else {
+        var i;
+        var isPrime = 1;
+        for (i = 2; i<=Math.sqrt(arg); i++) {
+            if (arg%i === 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        if (isPrime == 1) primes.push(arg);
+    }
+}
+console.log(primes);
