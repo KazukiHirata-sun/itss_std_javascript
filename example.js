b1b65be4b72069d1bae1b1d3d1b05738a49d3284
PhongPhamdt <phongpham.hedspi@gmail.com>
2020-03-15 04:03:17 +0700

    check prime

--- example.js
+++ example.js
@@ -3,3 +3,15 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+const isPrime = num => {
+  for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+      if(num % i === 0) return false 
+  return num > 1
+}
+
+const arr = [...process.argv.slice(2)]
+let result = []
+
+result = arr.filter(element => isPrime(element))
+
+console.log(result)
