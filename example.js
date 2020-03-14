6438e02d3e045d5d7bac5579f92e896fda887c8c
hoang cong thanh <hoangcongthanh1237d@gmail.com>
2020-03-14 16:25:09 +0700

    homework

--- example.js
+++ example.js
@@ -3,3 +3,22 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+var arr_size = prompt("Input the size of the array:");
+var i, j;
+var arr = Array();
+
+for(i=0; i<arr_size; i++){
+    var flag = true;
+    value = prompt("Input the value of number "+(i+1));
+    if(value > 2){
+        for(j = 2; j < Math.sqrt(value); j++){
+            if(value % j == 0){
+                flag = false;
+                break;
+            }
+        }
+        if(flag == true)
+            arr.push(value);
+    }
+}
+alert("Prime number in your array: " + arr);
