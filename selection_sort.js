64b0677139b31a2a4b93efefed7c8ddd6f0dc232
Lê Thanh Nam <47497720+lethanhnam46@users.noreply.github.com>
2020-03-14 23:58:09 +0700

    レ・タイん・ナム（20166475）問題２（火1）
    レ・タイん・ナム（20166475）問題２（火1）

---
+++ selection_sort.js
@@ -0,0 +1,19 @@
+function selectionSort(array) {
+    for(let i = 0; i < array.length; i++) {
+        let min = i;
+        for(let j = i + 1; j < array.length; j++) {
+            if(array[j] < array[min]) {
+                min = j;
+            }
+        }
+        if(i !== min) {
+            [array[i], array[min]] = [array[min], array[i]]
+        }
+    }
+    return array;
+}
+(function test() {
+    const testArray = [123, 2, 121, 2, 23]
+
+    console.log(selectionSort(testArray))
+})()
