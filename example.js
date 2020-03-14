4696ebfc9de9dc15983fb1d829dbbf7df05ac0fc
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-03-15 01:06:28 +0700

    feat: prime number checking in command line

--- example.js
+++ example.js
@@ -1,5 +1,22 @@
 #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
 // このファイルを修正して、プルリクエストしてください。
+
+const input = [];
+let result = [];
+
+input.push(...process.argv.slice(2));
+
+input.forEach(element => {
+  let isPrime = true;
+
+  if (element < 2) return;
+
+  for (let i = 2; i <= element / 2; i++) if (element % i == 0) isPrime = false;
+
+  if (isPrime) result.push(element);
+});
+
+console.log(result);
