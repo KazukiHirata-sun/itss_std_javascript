be1ee82a05b20888ceaa9ddf82374e1c89f74eb3
phamchien98 <phamchien22598hn@gmail.com>
2020-03-14 19:02:59 +0700

    mondai2

--- example.js
+++ example.js
@@ -3,3 +3,12 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+const arr=[123 2 121 2 23];
+arr.sort(function(a, b){
+    if (a > b)
+        return 1;
+    if (a < b)
+        return -1;
+    return 0
+});
+console.log(arr);
