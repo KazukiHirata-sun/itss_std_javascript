2d9cf76c2e14185ccecedcc02a73a670a73dd51d
quoctoanpk2511 <quoctoanpk2511@gmail.com>
2020-03-14 16:55:10 +0000

    first commit

--- example.js
+++ example.js
@@ -1,5 +1,18 @@
-#!/usr/bin/env node
-
 'use strict';
 
-// このファイルを修正して、プルリクエストしてください。
+var arr=[];
+var i = 0;
+
+for (const arg of process.argv) {
+    if (i > 1) {
+        arr.push(arg);
+    }
+    i++;
+}
+
+function sortArray() { 
+  arr.sort() 
+  console.log(arr); 
+} 
+ 
+sortArray();
