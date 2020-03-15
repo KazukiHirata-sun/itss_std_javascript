074e9f4fab86dabd9aa789be6f14894ccb648981
tuandang98 <49324133+tuandang98@users.noreply.github.com>
2020-03-15 17:46:32 +0700

    Update example.js

--- example.js
+++ example.js
@@ -3,3 +3,6 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+for (const arg of process.argv.slice(2).reverse().entries()){
+    console.log(process.argv.slice(2).length-arg[0]+"番目は"+arg[1]+"です");
+}
