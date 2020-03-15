732eab5033c478a1d6ed265e1cbab79d9506161e
Vinh <theten12@gmail.com>
2020-03-15 14:46:00 +0700

    課題３

--- example.js
+++ example.js
@@ -3,3 +3,23 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+// 課題３
+let argv = process.argv.slice(2)
+let result = []
+
+for (const arg of argv) {
+    let x = parseInt(arg), flag = true
+    if (!isNaN(x)) {
+        for(let i = 2; i <= x - 1; i++) 
+            if (x % i == 0) { 
+                flag = false; 
+                break; 
+            } 
+    }
+    
+    if (flag) {
+        result.push(x)
+    }
+}
+
+console.log(result)
