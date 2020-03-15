f70e476c30e2972ca2c0d22abc5259d57b71ddf7
huyen-tt <thanhhuyentran1998@gmail.com>
2020-03-15 10:36:53 +0000

    問題3

--- example.js
+++ example.js
@@ -3,3 +3,28 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+let nums = process.argv.slice(2);
+let prime = [];
+
+for (const num of nums) {
+    let a = parseInt(num);
+    let isprime = true;
+    let i =2;
+    
+    if (a<=1) isprime = false;
+    else{
+        if (a <= i) ;
+        else {
+            for(i; i<a ; i++){
+                if (a%i === 0){
+                    isprime = false;
+                    break;
+                }
+            }
+        }
+    }
+    
+    if(isprime) prime.push(a);
+}
+
+console.log(prime)
