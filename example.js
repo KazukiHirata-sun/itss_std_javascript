00808f09a014add055dd0960041a0c51a322bd63
hoangdq <hoangdq.vnist@gmail..com>
2020-03-16 01:34:24 +0700

    Dang Quang Hoang - 20161649

--- example.js
+++ example.js
@@ -2,4 +2,20 @@
 
 'use strict';
 
-// このファイルを修正して、プルリクエストしてください。
+//配列の宣言
+const arr = [];
+
+//配列に要素を追加
+for (const arg of process.argv.slice(2)){
+	arr.push(parseInt(arg, 10));
+}
+
+//小さい順にソート
+for (var i =0; i< arr.length-1; i++){
+	for(var j=i;j<arr.length; j++){
+		if(arr[j]<arr[i]) arr[j] = [arr[i], arr[i] = arr[j]][0]; 
+	}
+}
+
+//出力結果
+console.log(arr);
