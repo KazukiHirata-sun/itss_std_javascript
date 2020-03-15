e4fee8fc07a422c64f38c1eedf9710481258c2fa
hoang <vinhyenvodoi98@gmail.com>
2020-03-15 20:31:14 +0700

    mondai3

--- example.js
+++ example.js
@@ -3,3 +3,18 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+
+const isPrime = (num) => {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i++) if (num % i === 0) return false;
+  return num > 1;
+};
+
+const main = () => {
+  var array = process.argv.slice(2);
+
+  let doubled = array.filter((num) => isPrime(num));
+
+  console.log(doubled);
+};
+
+main();
