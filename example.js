84a82e3a9f3a32d2e1354ff424e5f153746c0f61
quanganh2310 <ailavip1st@gmail.com>
2020-03-15 07:27:51 +0000

    課題(1)

--- example.js
+++ example.js
@@ -2,4 +2,10 @@
 
 'use strict';
 
-// このファイルを修正して、プルリクエストしてください。
+var input = "anh hai long thin";
+var res = input.split(" ");
+res.reverse();
+res.forEach( function(str,index){
+    let num = res.length - index;
+    console.log(num + "番目は" + str + "です");
+});
