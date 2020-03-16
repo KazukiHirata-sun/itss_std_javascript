cc87e4de889fbda42cb11f6c44a626d6702757f4
フン・ティ・チャン <trangpt@datascience.com.vn>
2020-03-16 11:11:52 +0700

    問題１

--- example.js
+++ example.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
 // このファイルを修正して、プルリクエストしてください。
+let x = process.argv.slice(2).length;
+for (const arg of process.argv.slice(2).reverse()) {
+  console.log(`${x} 番目は${arg}です。`);
+  x--;
+}
