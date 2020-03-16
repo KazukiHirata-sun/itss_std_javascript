2b895febe1ff3c161a93e9220362a7675801c6f0
Dang Hoa <hoadang0107@gmail.com>
2020-03-16 16:33:15 +0000

    BTVN

--- example.js
+++ example.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 
 'use strict';
+let temp =[] ;
+for( let i = 2; i<process.argv.length; i++){
+    temp.push(process.argv[i]);
+}
+for(let i = temp.length-1; i >=0; i--){
+    console.log((i+1)+"番目は"+temp[i]+"です。");
+    
+}
 
 // このファイルを修正して、プルリクエストしてください。
