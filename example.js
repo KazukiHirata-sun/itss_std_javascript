b6157004936065aead5571a51454f5420902aa08
quangddoo <36820098+quangddoo@users.noreply.github.com>
2020-03-16 10:27:16 +0700

    Update homework

--- example.js
+++ example.js
@@ -3,3 +3,16 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+
+var array = process.argv.slice(2);
+array = array.filter((n) => {
+  if (n<2) return false;
+  else if (n==2) return true;
+    else {
+      for (let i = 2, s = Math.sqrt(n); i <= s; i++) {
+        if (n % i === 0) return false;
+      }
+    }
+  return true;
+});
+console.log(array);
