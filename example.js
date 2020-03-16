23377973dce32b2385bd3aca5bccec83a9908192
duonglh26 <62134845+duonglh26@users.noreply.github.com>
2020-03-16 23:09:40 +0700

    Update example.js

--- example.js
+++ example.js
@@ -2,4 +2,18 @@
 
 'use strict';
 
-// このファイルを修正して、プルリクエストしてください。
+let arrNum = process.argv.slice(2).map(el => parseInt(el));
+
+isPrime = num => {
+  if (num < 2) return false;
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+arrNum = arrNum.filter(el => isPrime(el));
+console.log(arrNum);
