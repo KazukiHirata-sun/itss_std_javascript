69ec649d88ca03afe3beb30cd2cf57a8c1c99e90
hoangbkcntt98 <20166135@student.hust.edu.vn>
2020-03-16 13:47:41 +0000

    exercise 3

--- example.js
+++ example.js
@@ -2,4 +2,17 @@
 
 'use strict';
 
-// このファイルを修正して、プルリクエストしてください。
+var A = process.argv.slice(2);
+for(var i=0; i<A.length; i++) { 
+    A[i] = parseInt(A[i], 10); 
+}
+A = A.filter((n) => {
+  if (n<2) return false
+    else {
+      for (var i = 2; i*i <= n; i++) {
+        if (n % i === 0) return false;
+      }
+    }
+  return true;
+});
+console.log(A);
