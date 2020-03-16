40445a73a0583bfbe2b54ea21b14f65546f30cb8
dantokoro <duongdang0508@gmail.com>
2020-03-16 15:15:36 +0000

    kadai 3

--- example.js
+++ example.js
@@ -1,5 +1,31 @@
 #!/usr/bin/env node
 
 'use strict';
-
+function test_prime(n)
+{
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;  
+  }
+}
+var arr = [];
+var i=0;
+for (const arg of process.argv.slice(2)) {
+    if(test_prime(parseInt(arg))) arr[i++] = parseInt(arg);
+}
+console.log(arr);
 // このファイルを修正して、プルリクエストしてください。
