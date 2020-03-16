7a20350e6b9c6eb49b29cade253d0b22aa2127b9
nga20167304 <nga20167304>
2020-03-16 02:25:02 +0000

    問題1

--- example.js
+++ example.js
@@ -3,3 +3,8 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+let x=process.argv.slice(2).length;
+for (const arg of process.argv.slice(2).reverse()) {
+    console.log(`${x} 番目は${arg}です。`);
+    x--;
+}
