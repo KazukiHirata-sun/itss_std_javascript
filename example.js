ee1a4ba666d4766bd1d2ed2cac73c7088b00450e
= <=>
2020-03-16 13:51:39 +0700

    ITSS

--- example.js
+++ example.js
@@ -1,5 +1,17 @@
-#!/usr/bin/env node
+﻿#!/usr/bin/env node
 
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+var argv = process.argv.slice(2);
+var result = [];
+loop: for (const arg of argv) {
+    let i = 2;
+    while (i < arg) {
+        if (arg % i === 0)
+            continue loop;
+        i++;
+    }
+    result.push(arg);
+}
+console.log(result);
