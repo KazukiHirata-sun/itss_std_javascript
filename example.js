fa5cb0e3e129e2bc46a55ec3985f92f077192aee
tuna <tu.nguyen@sotatek.com>
2020-03-16 23:05:57 +0700

    submmit exercise 3

--- example.js
+++ example.js
@@ -3,3 +3,39 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+
+/**
+ * 課題３
+ * 名前：グェン・アィン・トゥーン
+ *　学籍番号： 20164470
+ */
+
+const primeNumberFilter = (inputNumbers) => {
+  let primeNumbers = [];
+
+  inputNumbers.forEach(number => {
+    if(number < 2)
+      return;
+    else {
+      let i = 2;
+      do {
+        if (i == number) {
+          primeNumbers.push(number);
+          return;
+        } else if (number % i == 0) {
+          return;
+        } else {
+          i += 1;
+        }
+      } while (i <= number);
+    }
+    return; 
+  });
+
+  return primeNumbers;
+}
+
+const primeNumbers = primeNumberFilter(process.argv.slice(2));
+console.log(primeNumbers);
+
+
