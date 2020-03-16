e40821a7480c32fa0e571bb8016926854666caf6
lamnv <lamnv@datascience.com.vn>
2020-03-16 10:28:09 +0700

    comitto

--- example.js
+++ example.js
@@ -3,3 +3,17 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+const isPrime = num => {
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if(num % i === 0) return false; 
+    return num > 1;
+}
+
+let argv = process.argv.slice(2)
+
+const result = argv.filter((v) => {
+    if(!isPrime(v)) return false
+    return true
+})
+
+console.log(result)
