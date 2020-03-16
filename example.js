55c7cf6fee5637a209000cd7506ffa1d140b49f1
Nhocden <47529817+Nhocden@users.noreply.github.com>
2020-03-16 23:06:55 +0700

    課題(1)

--- example.js
+++ example.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
 'use strict';
+var j=process.argv.length-1;
+process.argv.reverse();
+for(const i of process.argv){
+    console.log(`${j}番目は${i}です`);
+    j-=1;
+    if(j<=0)break;
+}
 
 // このファイルを修正して、プルリクエストしてください。
