79323470a958b0c26d4aac1592af6fb3f086e77d
hieu <hieubkls98@gmail.com>
2020-03-16 16:02:34 +0700

    btvn

--- example.js
+++ example.js
@@ -1,5 +1,17 @@
-#!/usr/bin/env node
-
 'use strict';
 
+// このファイルを修正して、プルリクエストしてください。 
 // このファイルを修正して、プルリクエストしてください。
+
+//配列の宣言
+var res = []
+
+//配列に要素を追加
+for(const arg of process.argv.slice(2)) {
+  res.push(arg)
+}
+
+//最後の要素から配列を参照し、必要な形式で印刷
+for(let i = res.length - 1; i >= 0; i--) {
+  console.log(i+1 + '番目' + res[i] + 'です')
+} 
