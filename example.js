dcf95aae8cec33d3727da24c090f797c35997655
john3connor <mrtranbu.2605@gmail.com>
2020-03-16 18:23:45 +0700

    selection sort

--- example.js
+++ example.js
@@ -2,4 +2,41 @@
 
 'use strict';
 
-// このファイルを修正して、プルリクエストしてください。
+var arrStr = process.argv.slice(2);
+
+function selectionSort(arr) {
+    let minIndex, temp,
+        len = arr.length;
+
+    for (let i = 0; i < len; i++) {
+        minIndex = i;
+        // find min number
+        for (let  j = i+1; j < len; j++) {
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // swap
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+    return arr;
+}
+
+if(arrStr.length === 0){
+    console.log("No number from command line !")
+}
+var arrInt = [];
+for (const arg of arrStr) {
+    if(isNaN(arg)){
+        console.log("Invalid number !")
+        return;
+    }
+
+    let x = parseInt(arg, 10);
+    arrInt.push(x);
+}
+
+console.log(selectionSort(arrInt));
