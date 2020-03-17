e05345fc8a7856a6e8e4eee6c26b784302156310
haianhdt98 <phamhaianhdt98@gmail.com>
2020-03-17 13:56:09 +0000

    finish

--- example.js
+++ example.js
@@ -3,3 +3,33 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+var array = [];
+
+function check_prime(n){
+    var flag = true;
+    if(n < 2){
+        flag = false;
+    }
+    else if(n == 2){
+        flag = true;
+    }
+    else{
+        var i;
+        var m = Math.sqrt(n);
+        for(var i = 3; i < m; i++){
+            if(n % i == 0){
+                flag = false;
+                break;
+            }
+        }
+    }
+
+    if(flag == true) {
+        array.push(n);
+    }
+}
+
+for (const arg of process.argv.slice(2)) {
+    check_prime(arg);
+}
+console.log(array);
