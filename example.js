bca5cf906773a5b3eb6d6fff4d2395a570ed4439
Nguyen Kien <kiennd010498@gmail.com>
2020-03-17 08:12:21 +0700

    submit homework

--- example.js
+++ example.js
@@ -3,3 +3,14 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+for (const arg of process.argv.slice(2)) {
+  if(isPrime(parseInt(arg))) {
+    console.log(arg);
+  }
+}
+
+function isPrime(num) {
+  for(let i = 2; i < Math.sqrt(num); i++)
+    if(num % i === 0) return false;
+  return num > 1;
+}
