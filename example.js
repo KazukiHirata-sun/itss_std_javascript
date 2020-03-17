5696a1f4403ad59c653cfb3ad5a18f7213bfcf90
Khanhly30 <53520159+khanhlyvu30@users.noreply.github.com>
2020-03-17 23:22:16 +0700

    Update example.js

--- example.js
+++ example.js
@@ -1,5 +1,18 @@
 #!/usr/bin/env node
 
 'use strict';
+let arrNum = process.argv.slice(2).map(el => parseInt(el));
 
-// このファイルを修正して、プルリクエストしてください。
+isPrime = num => {
+  if (num < 2) return false;
+
+  for (let i = 2; i <= num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+arrNum = arrNum.filter(el => isPrime(el));
+console.log(arrNum);
