dd4d9f4f0f8683c5cf932d714e6ee9a0ddf8a386
Vu Duc Nguyen <41777964+nguyenvd27@users.noreply.github.com>
2020-03-17 21:32:56 +0700

    Update example.js

--- example.js
+++ example.js
@@ -11,9 +11,9 @@ for( let arg of process.argv.slice(2)){
 for( let i = 0; i < array.length; i++){
     for(let j = 0; j < array.length - i -1; j++){
         if(array[j] > array[j+1]){
-            let temp = array[j];
-			      array[j] = array[j + 1];
-			      array[j + 1] = temp;
+        	let temp = array[j];
+		array[j] = array[j + 1];
+		array[j + 1] = temp;
         }
     }
 }
