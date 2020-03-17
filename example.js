46ef000ac0cd35fe5fa0ee27f816679a79dd2860
duonglh26 <62134845+duonglh26@users.noreply.github.com>
2020-03-18 00:02:55 +0700

    Update example.js

--- example.js
+++ example.js
@@ -2,10 +2,10 @@
 
 'use strict';
 
-let arrNum = process.argv.slice(2).map(el => parseInt(el));
+let arrNum = process.argv.slice(2).map(el => Number(el));
 
 isPrime = num => {
-  if (num < 2) return false;
+  if (num < 2 || !Number.isInteger(num)) return false;
 
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
