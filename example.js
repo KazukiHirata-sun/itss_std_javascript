83ee740d7bd7ec959087f0af90c7747f3297df29
Bunbokun <PineCXp@gmail.com>
2020-03-17 17:27:12 +0000

    nop bt tuan 1 - bai 2

--- example.js
+++ example.js
@@ -1,5 +1,33 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'; 
+
+var arrStr = process.argv.slice(2);
+
+let bubbleSort = (inputArr) => {
+    let len = inputArr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (inputArr[j] > inputArr[j + 1]) {
+                let tmp = inputArr[j];
+                inputArr[j] = inputArr[j + 1];
+                inputArr[j + 1] = tmp;
+            }
+        }
+    }
+    return inputArr;
+};
+
+var arrInt = [];
+for (const arg of arrStr) {
+    if(isNaN(arg)){
+        return;
+    }
+    
+    let x = parseInt(arg, 10);
+    arrInt.push(x);
+}
+console.log("~~小さい順に並び替える~~");
+console.log(bubbleSort(arrInt));
 
 // このファイルを修正して、プルリクエストしてください。
