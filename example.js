fef48ffb042f05502fadc7572d698e973687249d
Le Ba Thien <38976540+boummm@users.noreply.github.com>
2020-03-17 13:31:59 +0700

    Add files via upload

--- example.js
+++ example.js
@@ -2,4 +2,17 @@
 
 'use strict';
 
-// このファイルを修正して、プルリクエストしてください。
+var arr = process.argv.slice(2);
+for(var i=0; i<arr.length; i++) { 
+    arr[i] = parseInt(arr[i], 10); 
+}
+arr = arr.filter((n) => {
+  if (n<2) return false
+    else {
+      for (var i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) return false;
+      }
+    }
+  return true;
+});
+console.log(arr);
