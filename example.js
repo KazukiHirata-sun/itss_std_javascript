e786dbb527bde8963e77345a35868eeb0fb74d3b
phanngoccuong <48021736+phanngoccuong@users.noreply.github.com>
2020-03-17 22:19:19 +0700

    Update example.js

--- example.js
+++ example.js
@@ -3,3 +3,20 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+var arr = []
+
+for( let a of process.argv.slice(2)){
+    arr.push(parseInt(a))
+}
+
+for( let x = 0; x < arr.length; x++){
+    for(let y = 0; y < arr.length - x -1; y++){
+        if(arr[y] > arr[y+1]){
+            let temp = arr[y];
+			arr[y] = arr[y + 1];
+			arr[y + 1] = temp;
+        }
+    }
+}
+
+console.log(arr);
