bc0d2a2fdfab3e6d2a9edb0cc2d38825c9a34797
Nguyen Hung <48485798+nguyenhung98bk@users.noreply.github.com>
2020-03-17 22:34:17 +0700

    Update example.js

--- example.js
+++ example.js
@@ -3,3 +3,20 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+var array = []
+
+for( let arg of process.argv.slice(2)){
+    array.push(parseInt(arg))
+}
+
+for( let i = 0; i < array.length; i++){
+    for(let j = 0; j < array.length - i -1; j++){
+        if(array[j] > array[j+1]){
+            let temp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = temp;
+        }
+    }
+}
+
+console.log(array)
