abaf04f269f2d34b3aa64db1184717ec6d2ce6bb
sockonlonton <thehungklk@gmail.com>
2020-03-17 22:20:12 +0700

    done

--- example.js
+++ example.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
 'use strict';
+const result = []
+for (const arg of process.argv) {
+    result.push(arg)
+}
 
-// このファイルを修正して、プルリクエストしてください。
+result.reverse();
+result.map((res, index) => {
+    let i = result.length - index
+    console.log(i + "番目は" + res + "です")
+})
+
