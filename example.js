297cfca89432171937fef08e7c9f45ba801a4f3a
trungchoe <55183259+trungchoe@users.noreply.github.com>
2020-03-17 21:30:00 +0700

    Update example.js

--- example.js
+++ example.js
@@ -1,5 +1,22 @@
 #!/usr/bin/env node
 
 'use strict';
+var arr = []
+
+for( let a of process.argv.slice(2)){
+    arr.push(parseInt(a))
+}
+
+for( let i = 0; i < arr.length; i++){
+    for(let j = 0; j < arr.length - i -1; j++){
+        if(arr[j] > arr[j+1]){
+            let temp = arr[j];
+			arr[j] = arr[j + 1];
+			arr[j + 1] = temp;
+        }
+    }
+}
+
+console.log(arr)
 
 // このファイルを修正して、プルリクエストしてください。
