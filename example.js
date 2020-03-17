100321bae092f3bea5dd960663d3015229c6a148
HanhNguyen98 <hanh.shin.1998@gmail.com>
2020-03-17 21:40:24 +0700

    complete 課題2

--- example.js
+++ example.js
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
 // このファイルを修正して、プルリクエストしてください。
+const input = process.argv.slice(2)
+var a = [];
+var numberIsPrime = number => {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.floor(Math.sqrt(number)); i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+};
+for(const number of input) {
+    const convertedToInt = parseInt(number)
+    if(numberIsPrime(convertedToInt) === true) a.push(convertedToInt)
+}
+console.log(a)
+
+
