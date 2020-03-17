379cf1fd66b620067ff9f6511b43a938f58dabef
HieuNT1998 <trihieua2@gmail.com>
2020-03-17 20:18:20 +0700

    add source code

--- example.js
+++ example.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
 'use strict';
-
-// このファイルを修正して、プルリクエストしてください。
+//console.log(process.argv)
+var len = process.argv.length - 1;
+for (　; len > 1; len -- ){
+    console.log( (len - 1) + "番目は " + process.argv[len]+" です。");
+}
