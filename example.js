7091d881844d781b58f1aed7a666cc39f102d888
HikPb <p.ngocba@gmail.com>
2020-03-17 20:39:27 +0000

    Pham Ngoc Ba -20160300- 課題3

--- example.js
+++ example.js
@@ -3,3 +3,25 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+function printPrimeNum()
+{
+    let array = [];
+    for(var j = 2;j<process.argv.length;j++){
+        let flag = true;
+        let n = parseInt(process.argv[j]);
+        if(n <2) {
+            flag = false;
+        }else{
+            for (var i=2; i<n-1; i++){
+                if(n%i===0){
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if(flag === true) array.push(n);
+      }
+      console.log(array);
+}
+
+printPrimeNum();
