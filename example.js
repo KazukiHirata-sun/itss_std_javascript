5027bb6c1f35903d752812a526da383bb6b5d7dd
leminhquang11 <quangleminh.leminh@gmail.com>
2020-03-18 07:09:43 +0700

    Mondai 3-leminh quang 20163293

--- example.js
+++ example.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 
 'use strict';
+var strArray = process.argv.slice(2);
+var numArray = strArray.map(Number);
+
+numArray = numArray.filter((n) => {
+  if (n<2) return false
+    else {
+      for (var i = 2; i*i <= n; i++) {
+        if (n % i == 0) return false;
+      }
+    }
+  return true;
+});
+
+console.log(numArray);
+
 
 // このファイルを修正して、プルリクエストしてください。
