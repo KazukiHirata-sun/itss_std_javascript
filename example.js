1cc2235ca83c91de21a416b5939355aa23ff4f84
nguyendanhkhanh <55346036+nguyendanhkhanh@users.noreply.github.com>
2020-03-18 09:51:39 +0700

    Update example.js

--- example.js
+++ example.js
@@ -3,3 +3,17 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+main(argc, argv )  
+{
+   let N = argc;
+
+   if( N < 2 )
+   {
+      console.log("Ban nen cung cap mot tham so.");
+      return;
+   }
+   for(i = N -1; i>=1; i--) 
+
+      console.log("番目"+ i +argv[i]);
+   
+}
