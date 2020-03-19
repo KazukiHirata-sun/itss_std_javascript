6437b960cff406d7ca36dd8c58d95f2bfd58db0c
duonglh26 <62134845+duonglh26@users.noreply.github.com>
2020-03-19 21:09:15 +0700

    Submit mondai 5

--- example.js
+++ example.js
@@ -2,6 +2,21 @@
 
 'use strict';
 
+//Mondai 5
+let arrNum = process.argv[2].replace("+", "=").split("=");
+
+for (let i = 0; i <= 9; i++) {
+  let [firstNum, secondNum, thirdNum] = arrNum.map(num => Number(num.replace(/X/g, i)));
+
+  if (firstNum + secondNum === thirdNum) {
+    console.log(i);
+    break;
+  }
+
+  if (i == 9) console.log("NA");
+}
+
+/* Mondai 3
 let arrNum = process.argv.slice(2).map(el => Number(el));
 
 isPrime = num => {
@@ -17,3 +32,4 @@ isPrime = num => {
 
 arrNum = arrNum.filter(el => isPrime(el));
 console.log(arrNum);
+*/
