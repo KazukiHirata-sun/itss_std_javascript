8956711186ca2d4ac93ed44a9af91c4a3f64e4e3
phuonganhbs98 <phuonganhbs1312@gmail.com>
2020-03-19 17:48:18 +0000

    fixed

--- example.js
+++ example.js
@@ -2,4 +2,7 @@
 
 'use strict';
 
-// このファイルを修正して、プルリクエストしてください。
+var len = process.argv.length;
+for(var i=len-1; i>1; i--){
+    console.log((i-1) + "番目は"+process.argv[i]+"です");
+}
--- mondai1.js
+++ mondai1.js
@@ -4,5 +4,5 @@
 
 var len = process.argv.length;
 for(var i=len-1; i>1; i--){
-    console.log(process.argv[i]);
+    console.log((i-1) + "番目は"+process.argv[i]+"です");
 }
