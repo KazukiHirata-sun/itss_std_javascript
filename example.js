dba59f9623d2e5d0a4cb7200878409555739bf79
Nguyen Viet Ngoc Quang 20163315 <quang.nvn163315@sis.hust.edu.vn>
2020-03-19 23:06:49 +0700

    Debug

--- example.js
+++ example.js
@@ -1,11 +1,11 @@
-#!/usr/bin/env node
-
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+
 var argv = process.argv.slice(2);
 var result = [];
 loop: for (const arg of argv) {
+    if (arg < 2) continue;
     let i = 2;
     while (i < arg) {
         if (arg % i === 0)
