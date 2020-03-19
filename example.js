f48862f7946fbefd5a97a1b6a28d8e152e7bbe34
phamchien98 <phamchien22598hn@gmail.com>
2020-03-19 21:55:22 +0700

    fix command line

--- example.js
+++ example.js
@@ -1,14 +1,8 @@
-#!/usr/bin/env node
-
 'use strict';
+let arr = [];
+for (const arg of process.argv) {
+    arr.push(arg);
+}
 
-// このファイルを修正して、プルリクエストしてください。
-const arr=[123 2 121 2 23];
-arr.sort(function(a, b){
-    if (a > b)
-        return 1;
-    if (a < b)
-        return -1;
-    return 0
-});
+arr.sort(function(a, b) { return a-b; });
 console.log(arr);
