ef924f1f2007a6376201e7c14f4cd35c2472cc11
piapip <tho.ph153619@sis.hust.edu.vn>
2020-03-19 11:31:18 +0700

    Jamashinaide

--- example.js
+++ example.js
@@ -3,25 +3,3 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
-// 問題3：素数だけを取り出す
-for (const arg of process.argv) {
-  // 数字だけを読む
-  let n = arg
-  if (n <= 1 || isNaN(n)) {
-    // nより１小さいか、nが数字ではない場合何もしない
-  }　else if (n < 4 && n > 1) {
-    console.log(n)
-  } else {
-    let temp = 2
-    let counter = 0　//　素数ならcounterがまま変わらない
-    // 素数検出
-    for (temp = 2; temp <= Math.ceil(n/2); temp++) {
-      if(n % temp == 0) {
-        counter++
-        break
-      }
-    }
-    // 素数なら表示する
-    if(counter == 0) console.log(n)
-  }  
-}
