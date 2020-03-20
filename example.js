e1659e14e6b4fc1d959c9ac5b31c10c32d7c244e
zovivo <tmczovivo@gmail.com>
2020-03-20 09:08:00 +0000

    覆面算

--- example.js
+++ example.js
@@ -1,35 +1,46 @@
 #!/usr/bin/env node
 
-'use strict'; 
+'use strict';
+
+// このファイルを修正して、プルリクエストしてください。
+
+// return the numbers of the formula
+let splitRecipe = function (recipeStr){
+    let arrNum = [];
+    let arrStr1 = recipeStr.split("+");
+    let arrStr2 = arrStr1[1].split("=");
+    arrNum.push(parseInt(arrStr1[0], 10));
+    arrNum.push(parseInt(arrStr2[0], 10));
+    arrNum.push(parseInt(arrStr2[1], 10));
+    
+    return arrNum;
+}
 
 var arrStr = process.argv.slice(2);
+let recipeStr = arrStr[0]
+var arrValueOfX = [];
 
-let bubbleSort = (inputArr) => {
-    let len = inputArr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len; j++) {
-            if (inputArr[j] > inputArr[j + 1]) {
-                let tmp = inputArr[j];
-                inputArr[j] = inputArr[j + 1];
-                inputArr[j + 1] = tmp;
-            }
-        }
-    }
-    return inputArr;
-};
+if(recipeStr.includes('X')){
+    for(var x = 0; x <10; x++){
 
-if(arrStr.length === 0){
-    console.log("No number from command line !")
-}
-var arrInt = [];
-for (const arg of arrStr) {
-    if(isNaN(arg)){
-        console.log("Invalid number !")
-        return;
+        let xValueStr = x.toString();
+        let newRecipeStr = recipeStr.replace(/X/gi, xValueStr);
+        let arrTerm = splitRecipe(newRecipeStr);
+        
+        let term1 = arrTerm[0];
+        let term2 = arrTerm[1];
+        let term3 = arrTerm[2];
+        let sum = term1 + term2;
+        
+        if(sum == term3)
+            arrValueOfX.push(x);
     }
-    
-    let x = parseInt(arg, 10);
-    arrInt.push(x);
 }
 
-console.log(bubbleSort(arrInt));
+if(arrValueOfX.length > 1){
+    console.log("X has more than 1 Value !!!");
+}else if(arrValueOfX.length ==1){
+    console.log(arrValueOfX[0]);
+}else{
+    console.log("NA");
+}
