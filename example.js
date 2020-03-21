070e6122bbf8af82f26c2e5b5e84b5ec21b30a76
グエン・ドゥック・ドー <do.nd167131@sis.hust.edu.vn>
2020-03-21 17:30:11 +0000

    課題４

--- example.js
+++ example.js
@@ -3,3 +3,7 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+var date1 = new Date(process.argv[2], process.argv[3]-1, process.argv[4]);
+var date2 = new Date(process.argv[5], process.argv[6]-1, process.argv[7]);
+var range = Math.abs(date2.getTime() - date1.getTime());
+console.log("result:", range/(1000*3600*24));
