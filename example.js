9e6eb2cf8790325c34ee1137d0aa267550984439
tiennguyen <tiennguyenbka198@gmail.com>
2020-03-21 22:25:22 +0700

    Done kadai5

--- example.js
+++ example.js
@@ -2,4 +2,24 @@
 
 'use strict';
 
-// このファイルを修正して、プルリクエストしてください。
+// フォーマット:数字列+数字列=数字列
+var arg = process.argv.slice(2)[0];
+
+if ((arg.match(/X/g) || []).length <= 1) {
+    console.log('NA');
+    process.exit(1);
+}
+
+for (var i = 0; i <= 9; i++) {
+    var str = arg.replace(/X/g, i);
+    var substr = str.split(/[+=]+/);
+    substr[0] = Number(substr[0]);
+    substr[1] = Number(substr[1]);
+    substr[2] = Number(substr[2]);
+    if (substr[0] + substr[1] == substr[2]) {
+        console.log(i);
+        process.exit(1);
+    }
+}
+//最後、答えがない場合だ。
+console.log('NA');
