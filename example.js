fb0895998d38c0f82bf8069ad306bb32ce2b9405
hoangdq <hoangdq.vnist@gmail..com>
2020-03-23 02:19:56 +0700

    ダン・クアン・ホアン - 20161649 - 課題5

--- example.js
+++ example.js
@@ -2,20 +2,36 @@
 
 'use strict';
 
-//配列の宣言
-const arr = [];
+//課題5
 
-//配列に要素を追加
-for (const arg of process.argv.slice(2)){
-	arr.push(parseInt(arg, 10));
+let arrStrNum = process.argv[2].replace("+", "=").split("=");
+
+for (var i = 0; i <= 9; ++i) {
+	var [Num1, Num2, Num3] = arrStrNum.map(num => Number(num.replace(/X/g, i)));
+	if (Num1 + Num2 === Num3) {
+		console.log(i);
+    	break;
+    }
+    if (i == 9) console.log("NA");
 }
 
-//小さい順にソート
-for (var i =0; i< arr.length-1; i++){
-	for(var j=i;j<arr.length; j++){
-		if(arr[j]<arr[i]) arr[j] = [arr[i], arr[i] = arr[j]][0]; 
-	}
-}
 
-//出力結果
-console.log(arr);
+//課題2
+
+// //配列の宣言
+// const arr = [];
+
+// //配列に要素を追加
+// for (const arg of process.argv.slice(2)){
+// 	arr.push(parseInt(arg, 10));
+// }
+
+// //小さい順にソート
+// for (var i =0; i< arr.length-1; i++){
+// 	for(var j=i;j<arr.length; j++){
+// 		if(arr[j]<arr[i]) arr[j] = [arr[i], arr[i] = arr[j]][0]; 
+// 	}
+// }
+
+// //出力結果
+// console.log(arr);
