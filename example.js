27bfa4711198dbc4a06349a40a34cdbc35de5463
hoangbkcntt98 <20166135@student.hust.edu.vn>
2020-03-22 16:08:24 +0000

    exercise 6

--- example.js
+++ example.js
@@ -3,16 +3,7 @@
 'use strict';
 
 var A = process.argv.slice(2);
-for(var i=0; i<A.length; i++) { 
-    A[i] = parseInt(A[i], 10); 
-}
-A = A.filter((n) => {
-  if (n<2) return false
-    else {
-      for (var i = 2; i*i <= n; i++) {
-        if (n % i === 0) return false;
-      }
-    }
-  return true;
-});
-console.log(A);
+A = A.map(Number);
+var max = Math.max.apply(null,A);
+var min = Math.min.apply(null,A);
+console.log(Math.floor((max+min)/2));
