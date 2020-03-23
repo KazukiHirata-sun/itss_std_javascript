be31ec679459823a9ee4132e61cbddd4d642fb79
Nguyen Hung <48485798+nguyenhung98bk@users.noreply.github.com>
2020-03-23 20:25:36 +0700

    課題6

--- example.js
+++ example.js
@@ -8,15 +8,19 @@ var array = []
 for( let arg of process.argv.slice(2)){
     array.push(parseInt(arg))
 }
-
-for( let i = 0; i < array.length; i++){
-    for(let j = 0; j < array.length - i -1; j++){
-        if(array[j] > array[j+1]){
-            let temp = array[j];
-			array[j] = array[j + 1];
-			array[j + 1] = temp;
-        }
+	let Min = array[0];
+	let Max = array[0];
+for( let i = 1; i < array.length; i++){
+    if (Min>array[i]) {
+    	Min = array[i];
+    }
+    if(Max<array[i]){
+    	Max = array[i];
     }
 }
-
-console.log(array)
+if((Min+Max)%2==0){
+	console.log((Min+Max)/2);
+}
+else{
+	console.log((Min+Max-1)/2);
+}
