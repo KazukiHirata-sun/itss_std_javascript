102e90d79fdc02b8d4715546df0df8e007a36689
ThanhThao44 <thao441997@gmail.com>
2020-03-23 04:43:29 +0000

    課題6

--- example.js
+++ example.js
@@ -5,14 +5,28 @@
 // このファイルを修正して、プルリクエストしてください。
 var arr = [];
 var n = 0;
-for (const arg of process.argv.slice(2)) {
- arr[n] = arg;
- n++;
-}
-for (var i = (n-1); i>=0; i--){
-    console.log((i+1) + "番目は" + arr[i] + "です");
-    if(i===0){
-        break;
+
+function sum(arr){
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++){
+            sum += arr[i];
     }
-    console.log();
+    return sum;
 }
+ 
+for (const arg of process.argv.slice(2)) {
+    arr[n] = Number(arg);
+    n++;
+}
+
+var s = sum(arr);
+var a = (s%n)/n;
+
+if(a > 0.5){
+    console.log(Math.floor(s/n) + 1);
+}
+else{
+    console.log(Math.floor(s/n));
+}
+
+
