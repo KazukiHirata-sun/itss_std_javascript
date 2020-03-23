505df980e968aaa16c37d9315fcc554884885a09
thuyquynhcao <caoquynh2@gmail.com>
2020-03-23 08:13:34 +0000

    bt

--- example.js
+++ example.js
@@ -2,10 +2,26 @@
 
 'use strict';
 
-// このファイルを修正して、プルリクエストしてください。
-function sum(a, b) {
-var result = a + b;
-return result;
+
+var array = process.argv[2].split('+');
+var string1 = array[0];
+var string2 = array[1].split('=')[0], string3 = array[1].split('=')[1];
+
+var f, g, h;
+var x, counter = 0;
+
+for(let i = 0; i < 10; i++){
+    f = parseInt(string1.replace(/X/g, i));
+    g = parseInt(string2.replace(/X/g, i));
+    h = parseInt(string3.replace(/X/g, i));
+    if( f+g == h){
+        x = i;
+        counter++;
+    }
 }
-var answer = sum(1,2);
-console.log(answer);
+
+if(counter != 1){
+    console.log('NA');
+} else {
+    console.log(x);
+}
