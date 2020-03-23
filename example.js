99261438274068ea3e6f471a03bf5be455a0be58
= <=>
2020-03-23 20:48:24 +0700

    ITSS

--- example.js
+++ example.js
@@ -2,16 +2,8 @@
 
 // このファイルを修正して、プルリクエストしてください。
 
-var argv = process.argv.slice(2);
-var result = [];
-loop: for (const arg of argv) {
-    if (arg < 2) continue;
-    let i = 2;
-    while (i < arg) {
-        if (arg % i === 0)
-            continue loop;
-        i++;
-    }
-    result.push(parseInt(arg));
-}
-console.log(result);
+var A = process.argv.slice(2);
+A = A.map(Number);
+var max = Math.max.apply(null,A);
+var min = Math.min.apply(null,A);
+console.log(Math.floor((max+min)/2));
