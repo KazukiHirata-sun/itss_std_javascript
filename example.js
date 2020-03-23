eb1a132adef1f6afb287f12451c53f3ce1a1c0b8
poipoi0099 <56574097+poipoi0099@users.noreply.github.com>
2020-03-23 21:34:07 +0700

    Update example.js

--- example.js
+++ example.js
@@ -3,3 +3,15 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+
+var date1 = (process.argv[2])+'-'+(process.argv[3])+'-'+(process.argv[4]);
+var date2 = (process.argv[5])+'-'+(process.argv[6])+'-'+(process.argv[7]);
+
+console.log(date1);
+console.log(date2);
+
+var dt1 = new Date(date1);
+var dt2 = new Date(date2);
+
+var date_diff_indays = Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+console.log(date_diff_indays);
