5df7e89d5c0bda64ece2a893efb675a8245f9f4d
hoang <vinhyenvodoi98@gmail.com>
2020-03-24 20:30:25 +0700

    mondai 6

--- example.js
+++ example.js
@@ -4,17 +4,11 @@
 
 // このファイルを修正して、プルリクエストしてください。
 
-const isPrime = (num) => {
-  for (let i = 2, s = Math.sqrt(num); i <= s; i++) if (num % i === 0) return false;
-  return num > 1;
-};
-
 const main = () => {
   var array = process.argv.slice(2);
 
-  let doubled = array.filter((num) => isPrime(num));
-
-  console.log(doubled);
+  array.forEach((element) => parseInt(element));
+  console.log(Math.floor((Math.max(...array) + Math.min(...array)) / 2).toString());
 };
 
 main();
