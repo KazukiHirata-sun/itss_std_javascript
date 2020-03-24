941e364140e13355e4cb25b645055f906d9d44d5
huyen-tt <thanhhuyentran1998@gmail.com>
2020-03-24 01:45:33 +0000

    問題6

--- example.js
+++ example.js
@@ -3,28 +3,39 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
-let nums = process.argv.slice(2);
-let prime = [];
 
-for (const num of nums) {
-    let a = parseInt(num);
-    let isprime = true;
-    let i =2;
-    
-    if (a<=1) isprime = false;
-    else{
-        if (a <= i) ;
-        else {
-            for(i; i<a ; i++){
-                if (a%i === 0){
-                    isprime = false;
-                    break;
-                }
-            }
-        }
-    }
-    
-    if(isprime) prime.push(a);
+function check_room(rooms) {
+  for (const room of rooms) {
+    let a = parseInt(room);
+    if (a > 20) 
+        return 0;
+  }
+  return 1;
 }
 
-console.log(prime)
+function find_med(argv){
+    let min = argv[0];
+    let max = argv[0];
+    var i=1;
+    for (i=1;i<argv.length; i++){
+        if(argv[i]>max)
+            max=argv[i];
+        else {
+            if(argv[i]<min)
+            min=argv[i];
+        }
+    }
+    max = parseInt(max);
+    min = parseInt(min);
+    let med = (max+min)/2;
+    med = parseInt(med);
+    return med;
+}
+
+let argv = process.argv.slice(2);
+if (check_room(argv) === 0) {
+  console.log(`Room's number have to smaller than 21`);
+} else {
+  var med = find_med(argv);
+  console.log(`${med}`);
+}
