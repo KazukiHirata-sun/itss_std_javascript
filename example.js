a624a3cdb3a052418341ab4a501de2193113f7f9
TNT-Escanor <powsasvkl@gmail.com>
2020-03-24 23:49:04 +0700

    HW

--- example.js
+++ example.js
@@ -3,23 +3,15 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
-// 課題３
-let argv = process.argv.slice(2)
-let result = []
-
-for (const arg of argv) {
-    let x = parseInt(arg), flag = true
-    if (!isNaN(x)) {
-        for(let i = 2; i <= x - 1; i++) 
-            if (x % i == 0) { 
-                flag = false; 
-                break; 
-            } 
-    }
-
-    if (flag) {
-        result.push(x)
-    }
+// 課題6
+var argv = process.argv.slice(2)
+var min = argv[0];
+var max = argv[0];
+for (let i = 1; i < argv.length; i++) {
+    if (argv[i] < min)
+        min = argv[i];
+    else if (argv[i] > max)
+        max = argv[i];
 }
-
+var result = (parseInt(min) + parseInt(max)) / 2;
 console.log(result)
