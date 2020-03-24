abae36ab9e4e0ce6261c5cfa9e4f293f89957f01
dinhson2905 <dinhson2905@gmail.com>
2020-03-24 19:21:14 +0700

    add comment

--- mondai4.js
+++ mondai4.js
@@ -1,3 +1,4 @@
+//配列の宣言
 var res = []
 
 if (process.argv.length != 8) {
@@ -5,11 +6,13 @@ if (process.argv.length != 8) {
     process.exit(-1)
 }
 
+//配列に要素を追加
 for (const arg of process.argv.slice(2)) {
     res.push(parseInt(arg))
 }
-
+//日付の宣言
 var date1 = new Date(res[0], res[1], res[2])
 var date2 = new Date(res[3], res[4], res[5])
 
+//結果
 console.log((parseInt(date2 - date1)/(24 * 60 * 60 * 1000)))
