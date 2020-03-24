1cc394df345353a01efb73ed612a9172def0a3e8
Hinh1009 <50358382+Hinh1009@users.noreply.github.com>
2020-03-25 00:41:40 +0700

    Add files via upload

---
+++ mondai6.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var A = process.argv.slice(2);
+for(var i=0; iA.length; i++) {
+    A[i] = parseInt(A[i]);
+}
+A = A.filter((n) = {
+  if (n2n20) return false
+    else {
+      for (var i = 2; ii = n; i++) {
+        if (n % i === 0) return false;
+      }
+    }
+  return true;
+});
+console.log(A);
+
+var A = process.argv.slice(2);
+A = A.map(Number);
+var max = Math.max.apply(null,A);
+var min = Math.min.apply(null,A);
+
+function average(min, max) {
+  if ((min + max) % 2 == 0) return (min + max)  2;
+  else return (min + max - 1)  2;
+}
+var avg = average(min, max);
+console.log(`${avg}`);
