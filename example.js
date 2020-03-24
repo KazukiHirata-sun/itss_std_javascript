99fd77d6e686c3d9a36f9eef6d55da629e7e64bb
Bunbokun <PineCXp@gmail.com>
2020-03-24 16:09:56 +0000

    bai tap tuan 2 - bai 6

--- example.js
+++ example.js
@@ -2,32 +2,15 @@
 
 'use strict'; 
 
-var arrStr = process.argv.slice(2);
-
-let bubbleSort = (inputArr) => {
-    let len = inputArr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len; j++) {
-            if (inputArr[j] > inputArr[j + 1]) {
-                let tmp = inputArr[j];
-                inputArr[j] = inputArr[j + 1];
-                inputArr[j + 1] = tmp;
-            }
-        }
-    }
-    return inputArr;
-};
-
-var arrInt = [];
-for (const arg of arrStr) {
-    if(isNaN(arg)){
-        return;
-    }
-    
-    let x = parseInt(arg, 10);
-    arrInt.push(x);
+let argv = process.argv.slice(2);
+let max = parseInt(argv[0]);
+let min = parseInt(argv[0]);
+for (const arg of argv)
+{
+    let x = parseInt(arg);
+    if (x>max) max = x;
+    if (x<min) min =x;
 }
-console.log("~~小さい順に並び替える~~");
-console.log(bubbleSort(arrInt));
+console.log(Math.floor((max+min)/2));
 
 // このファイルを修正して、プルリクエストしてください。
