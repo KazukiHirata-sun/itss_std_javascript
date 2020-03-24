0b134c98714fdbf96622bd76b4cd302a9571d4c5
Ubuntu <ubuntu@ip-172-31-43-10.ec2.internal>
2020-03-24 17:08:19 +0000

    Phan Thi Thuy 20166816

--- example.js
+++ example.js
@@ -1,7 +1,5 @@
 'use strict'; 
 var numbers = process.argv.slice(2);
- 
-// Lấy giá trị lớn nhất và nhỏ nhất
 var max = Math.max.apply(Math, numbers); 
 var min = Math.min.apply(Math, numbers);
 var a = Math.floor((max+min)/2);
