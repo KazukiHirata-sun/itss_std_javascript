76d8cfb99734ca7eba32a0cec0b4fe2f9234985f
VP1927 <phamvietcuong2704@gmail.com>
2020-03-25 16:58:02 +0700

    changing multiple files name

--- phamvietcuong_20160579/phamvietcuong_20160579_mondai5.js
+++ phamvietcuong_20160579/phamvietcuong_20160579_mondai5.js
@@ -5,14 +5,16 @@
 // このファイルを修正して、プルリクエストしてください。
 
 function getNum(eqs){
-    return eqs.split(/[+=*-//]/);
+    return eqs.split(/[+=*-/]/);
 }
 
-function get(eqs){
-    return eqs.split(/^[+=*-//]/);
+function getPar(eqs){
+    return eqs.split(/[/number]/);
 }
 
 var myArgs = process.argv.slice(2);
 console.log(myArgs);
+console.log("this is right files")
 
 console.log(getNum(myArgs[0]))
+console.log(getPar(myArgs[0]))
