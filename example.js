dba31a1d369a841008baec4cf15b77d5bca474b5
ThanhThao44 <thao441997@gmail.com>
2020-03-27 11:21:31 +0000

    fix

--- example.js
+++ example.js
@@ -6,27 +6,12 @@
 var arr = [];
 var n = 0;
 
-function sum(arr){
-    var sum = 0;
-    for (var i = 0; i < arr.length; i++){
-            sum += arr[i];
-    }
-    return sum;
-}
- 
 for (const arg of process.argv.slice(2)) {
     arr[n] = Number(arg);
     n++;
 }
+arr.sort(function(a,b){return a-b});
 
-var s = sum(arr);
-var a = (s%n)/n;
-
-if(a > 0.5){
-    console.log(Math.floor(s/n) + 1);
-}
-else{
-    console.log(Math.floor(s/n));
-}
+console.log(Math.floor((arr[0]+arr[n-1])/2));
 
 
